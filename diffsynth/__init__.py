2fd02094066d83908a0315a83c10091a2a61c552
Seunghyun Shin <55797829+seunghyuns98@users.noreply.github.com>
2026-04-15 18:30:02 +0900

    Delete diffsynth directory

--- diffsynth/__init__.py
+++
@@ -1,6 +0,0 @@
-from .data import *
-from .models import *
-from .prompters import *
-from .schedulers import *
-from .pipelines import *
-from .controlnets import *
--- diffsynth/pipelines/__init__.py
+++
@@ -1,3 +0,0 @@
-from .wan_video import WanVideoPipeline
-from .wan_video_multimodal import WanVideoMultimodalPipeline
-from .wan_video_i2v import WanVideoI2VPipeline
